278c23fc2ef47485f41dfa2cd5b2a03fd5f11778
Qasim Wani <qasim31wani@gmail.com>
2020-06-08 04:47:54 -0400

    add more stats. basics

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -5,13 +5,22 @@ chrome.storage.sync.get("zen_daily_metrics", zen_data => {
     if (zen_data && zen_data.timeline) {
         var X = [],
             Y = [],
-            Y_prime = [];
-
+            Y_prime = [],
+            hours = {f : 0, nf : 0};
+        
         Object.keys(zen_data.timeline).map(function (key) {
             X.push(key);
-            Y.push(parseFloat(zen_data.timeline[key] / 3600).toPrecision(3));
-            Y_prime.push(parseFloat(( 57600 /** assuming 8 hours of sleep **/ - zen_data.timeline[key] ) / 3600 ).toPrecision(3))
+            let f = parseFloat(zen_data.timeline[key] / 3600).toPrecision(3),
+                nf = parseFloat(( 57600 /** assuming 8 hours of sleep **/ - zen_data.timeline[key] ) / 3600 ).toPrecision(3);
+            hours.f += parseFloat(f);
+            hours.nf += parseFloat(nf);
+            Y.push(f);
+            Y_prime.push(nf);
         });
+        document.getElementById("d").textContent = Y.length;
+        document.getElementById("f").textContent = hours.f.toPrecision(3);
+        document.getElementById("nf").textContent = hours.nf.toPrecision(3);
+        
         var myChart = new Chart(ctx, {
             type: 'line',
             data: {
