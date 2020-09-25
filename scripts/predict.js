aa5e66eb039b1ccfda2b3e50d0148caa2611a689
Qasim Wani <qasim31wani@gmail.com>
2020-09-26 00:02:13 +0300

    predict time init commit

---
+++ scripts/predict.js
@@ -0,0 +1,5 @@
+/**
+ * This file predicts the time the user will work for based on 
+ * their past activity.
+ * Implemented using OLS.
+ */
--- zoom.js
+++
@@ -1,6 +0,0 @@
-// The purpose of this class is to help solve some common inconveniences.
-// the first being creating Zoom meetings. This takes time because I have
-// to open the app, click create meeting, log in, etc. etc. Why not automate
-// the entire process.
-// that's what this file is trying to do.
-// todo...
