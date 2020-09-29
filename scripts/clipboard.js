34fb382259f3e9c142cf23fee13a3de482420a08
Qasim Wani <qasim31wani@gmail.com>
2020-09-29 16:58:57 +0300

    working commit for storage of clipboard

--- scripts/clipboard.js
+++ scripts/clipboard.js
@@ -6,7 +6,17 @@ $("ul").on("click", "li", function(){
 
 $("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500,function(){
-		$(this).remove();
+        chrome.storage.sync.get('vti_clipboard', data=>{
+            data = data.vti_clipboard;
+            if(data != undefined) //should never exist, but better be safe than sorry!
+            {
+                let index = data.indexOf($(this).text());
+                data.splice(index, 1);
+                chrome.storage.sync.set({'vti_clipboard':data}, ()=>{
+                    $(this).remove();
+                });
+            }
+        });
 	});
 	event.stopPropagation();
 });
@@ -15,8 +25,23 @@ $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
 		var $newItem = $(this).val();
 		if ($newItem.length > 3) {
-			$(this).val("");
-			$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + $newItem + "</li>")
+            $(this).val("");
+            chrome.storage.sync.get('vti_clipboard', data=>{
+                data = data.vti_clipboard;
+                console.log("load init", data);
+                if(data != undefined) //records exist
+                {
+                    data.push($newItem);
+                }
+                else
+                {
+                    data = [$newItem];
+                }
+                chrome.storage.sync.set({'vti_clipboard': data}, ()=>{
+                    $("ul").append("<li><span class='delete'><i class='fa fa-trash'></i></span>" + $newItem + "</li>")            
+                });
+            });
+			
 		} else {
 			$(this).val("");
 			$(this).attr("placeholder", "Need at least 4 characters!");			
@@ -36,3 +61,15 @@ function copyToClipboard(element) {
 	$(element).attr('title', 'Copied to clipboard!');
     $temp.remove();
 }
+
+// Load initial results with queries
+chrome.storage.sync.get('vti_clipboard', data=>{
+    data = data.vti_clipboard;
+    if(data != undefined)
+    {
+        data.forEach(item => {
+            
+            $("ul").append("<li><span class='delete'><i class='fa fa-trash'></i></span>" + item + "</li>")            
+        });
+    }
+});
